8ac45a782c27738c6774b7c321186e465f64f82e
Lukas Nepomuceno <nepomucenol0532@my.sduhsd.net>
2021-01-18 09:11:28 +0000

    Spent hours completing project. EC WIP

--- proj/encrypt/encrypt.c
+++ proj/encrypt/encrypt.c
@@ -3,13 +3,29 @@
 //
 
 #include <string.h>
-
 #include "encrypt.h"
 
 
 char CHARS[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 int CHARS_LEN = 62;
 
+int findIndex(char c) {
+  int index;
+  if(c >= 97 && c <= 122) {
+    // Lowercase ASCII range
+    // Subtract the ASCII value for correct index
+    index = c - 97;
+  }else if( c >= 65 && c <= 90) {
+    // Uppercase ASCII range
+    // Subtract ASCII index and move past the lowercase letters
+    index = c - 65 + 26;
+  }else if(c >= 48 && c <= 57) {
+    // Number ASCII range
+    // Subtract ASCII index and move past both upper and lower case letters
+    index = c - 48 + 52;
+  }
+  return index;
+}
 
 char shiftChar(char c, int shift, int direction)
 {
@@ -20,8 +36,35 @@ char shiftChar(char c, int shift, int direction)
   //
   // eg.
   //   shiftChar('c', 3, 1) : 'f'
-  //   shiftChar('S', 2, 0) : 'P'
+  //   shiftChar('s', 2, 0) : 'P'
   //   shiftChar('b', 3, 0) : '8'
+
+  // Index stores the location of the char in the CHARS array
+  int index;
+
+  // Right = positive = 1 and left = negative = 0
+  // So only left or 0 conditions must change sign
+  if(direction == 0) {
+    // Change sign to the left (negative)
+    shift = 0 - shift;
+  }
+
+  // Search for char location in the ASCII table
+  // Turned this section into a function to apply to EC
+  index = findIndex(c);
+
+  // Apply encryption shift
+  int result = index + shift;
+
+  // Use loops instead of ifs in case a shift key exceeds the char array size  
+  while(result < 0) {
+    result += CHARS_LEN;
+  }
+  while(result >= CHARS_LEN) {
+    result -= CHARS_LEN;
+  }
+  // Return the shifted char according to final location
+  return CHARS[result];
 }
 
 
@@ -60,4 +103,36 @@ void caesarDecrypt(char str[], int shift)
   decrypt(str, shifts, 1);
 }
 
+void vigDecipher(char str[], char keyword[], int encDec)
+{
+  // The same process occurs for vigEncrypt AND vigDecrypt
+  // The only difference is shifting forward or backward, denoted by encDec
 
+  // First, create the shift pattern based off of the keyword
+  int keywordSize = strlen(keyword);
+  int shiftPattern[keywordSize];
+  for (int i = 0; i < keywordSize; i++) {
+    shiftPattern[i] = findIndex(keyword[i]);
+  }
+
+  // Apply the corresponding part of the shift pattern to change the string
+  int strSize = strlen(str);
+  for (int i = 0; i < strSize; i++) {
+    int temp = i;
+    while(temp > strlen(keyword)) {
+      temp -= strlen(keyword);
+    }
+    str[i] = shiftChar(str[i], shiftPattern[temp], encDec);
+  }
+
+}
+
+void vigEncrypt(char message[], char keyword[])
+{
+  vigDecipher(message, keyword, 1);
+}
+
+void vigDecrypt(char message[], char keyword[])
+{
+  vigDecipher(message, keyword, 0);
+}
--- proj/student.c
+++ proj/student.c
@@ -12,10 +12,10 @@
 
 const char* STUFILE = "studentdata.txt";
 
-// keeps track of the number of students in the students array
+// Keeps track of the number of students in the students array
 int numStudents = 0;
 
-// holds the student pointer array
+// Holds the student pointer array
 Student* students[100] = {0};
 
 
@@ -24,52 +24,147 @@ void createStudent(char* fname, char* lname, int age, int id)
   // createStudent dynamically creates a Student on the heap and adds that
   // student to the student array
   //  - the firstName and lastName strings should be dynamically created
-  //    based on the size of the fname and lname args
+  //    based on the size of the fname and lname arg
+
+  // Follow instructions above for the current student using numStudents 
+  students[numStudents] = (Student*)malloc(sizeof(Student));
+  students[numStudents]->firstName = (char*)malloc(sizeof(fname));
+  students[numStudents]->lastName = (char*)malloc(sizeof(lname));
+  strcpy(students[numStudents]->firstName, fname);
+  strcpy(students[numStudents]->lastName, lname);
+  students[numStudents]->age = age;
+  students[numStudents]->id = id;
+
+  // Make sure to increment!
+  numStudents++;
 }
 
 
 void deleteStudent(Student* student)
 {
-  // free the memory associated with a student including the strings
+  // Free the memory associated with a student including the strings
+  free(student->firstName);
+  free(student->lastName);
+  free(student);
 }
 
 
 void deleteStudents()
 {
-  // iterate over the students array deleting every student and setting te pointer
+  // Iterate over the students array deleting every student and setting the pointer
   // values to 0 and adjusting the numStudents to 0
+  for(int i = 0; i < numStudents; i++) {
+    students[i]->firstName = 0;
+    students[i]->lastName = 0;
+    students[i]->age = 0;
+    students[i]->id = 0;
+    deleteStudent(students[i]);
+  }
+  numStudents = 0;
 }
 
 
 void saveStudents(int key)
 {
-  // save all students in the student array to a file 'studentdata.txt' overwriting
+
+  FILE* sf;
+  // Student file
+
+  sf = fopen("studentdata.txt", "w");
+  // Save all students in the student array to a file 'studentdata.txt' overwriting
   // any existing file
   //   - the format of the file is one line per student as follows fname lname age id:
   //       tom thumb 15 1234 
   //       james dean 21 2345 
   //       katy jones 18 4532 
+  if (sf) {
+    for(int i = 0; i < numStudents; i++) {
+      // Initialize temporary string storage variables
+      char ageTemp[4];
+      char idTemp[50];
+
+      // Stores the ints as strings. Was this necessary?
+      sprintf(ageTemp, "%d", students[i]->age);
+      sprintf(idTemp, "%ld", students[i]->id);
+
+      // Encrypt each aspect of the struct
+      caesarEncrypt(students[i]->firstName, key);
+      caesarEncrypt(students[i]->lastName, key);
+      caesarEncrypt(ageTemp, key);
+      caesarEncrypt(idTemp, key);
+
+      // Print each struct aspect to the file and record it to user
+      fprintf(sf, "%s %s %s %s\n", students[i]->firstName, students[i]->lastName, ageTemp, idTemp);
+      printf("Saving: %s %s %s %s\n", students[i]->firstName, students[i]->lastName, ageTemp, idTemp);
+    }
+
+    // Report number of students saved to the file
+    printf("Saved %d students\n", numStudents);
+    fclose(sf);
+  } 
 }
 
 
 void loadStudents(int key)
 {
-  // load the students from the data file overwriting all exisiting students in memory
+  // Load the students from the data file overwriting all existing students in memory
+  // Wipes everything to "overwrite"
+  deleteStudents();
+  FILE* sf;
+  sf = fopen("studentdata.txt", "r");
+  if (sf) {
+    while (1) {
+      // Initialize storage variables
+      char fname[50];
+      char lname[50];
+      char ageTemp[50];
+      char idTemp[50];
+      int age; long int id;
+
+      // Check for a student by looking for lines with four arguments
+      if(fscanf(sf, "%s %s %s %s\n", fname, lname, ageTemp, idTemp) == 4) {
+        // Looked it up--use sprintf to convert age and id to strings
+        // I get the feeling somehow that ints themselves should be able to pass into
+        //   the encrypt function with a bit of manipulation not like this. 
+        // Am I doing it right??
+        //sprintf(ageTemp, "%d", age);
+        //sprintf(idTemp, "%ld", id);
+
+        // Decrypt all aspects of the struct
+        caesarDecrypt(fname, key);
+        caesarDecrypt(lname, key);
+        caesarDecrypt(ageTemp, key);
+        caesarDecrypt(idTemp, key);
+
+        // Convert age and id back to numbers to recreate student.
+        // Again, I think I am supposed to do this without using atoi().
+        // If this were required, the instructions would mention it...
+        // ...right?
+        age = atoi(ageTemp);
+        id = atoi(idTemp);
+        createStudent(fname, lname, age, id);
+      }
+      else
+        break;
+    }
+    fclose(sf);
+    printf("Loaded %d students\n", numStudents);
+  }
 }
 
 
 void printStudent(Student* student)
 {
   printf("  Student: %s %s\n", student->firstName, student->lastName);
-  printf("    age: %d\n", student->age);
-  printf("    id: %ld\n", student->id); 
+  printf("    Age: %d\n", student->age);
+  printf("    ID: %ld\n", student->id); 
 }
 
 
 void printStudents()
 {
   for (int i = 0; i < numStudents; i++) {
-    printf("\n----- student %d ------\n", i);
+    printf("\n----- Student %d ------\n", i);
     printStudent(students[i]);
   }
 }
